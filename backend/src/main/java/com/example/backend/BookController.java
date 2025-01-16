efb9faf8a81c9d687b3c66a4d84733273e434462
eduardomer <eduardomesut@gmail.com>
2025-01-16 17:02:08 +0100

    CRUD Spring

--- backend/src/main/java/com/example/backend/BookController.java
+++ backend/src/main/java/com/example/backend/BookController.java
@@ -6,12 +6,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/books")
 public class BookController {
+
     @GetMapping
-    public List<Book> findAll(){
+    public List<Book> findAll() {
         System.out.println("FindAll");
         return List.of(
                 new Book(1L, "Book1"),
@@ -20,9 +25,27 @@ public class BookController {
                 new Book(4L, "Book4")
         );
     }
+
     @GetMapping("/{id}")
-    public Book findById(@PathVariable Long id){
+    public Book findById(@PathVariable Long id) {
         System.out.println("FindById");
         return new Book(id, "ejemplo");
+    }
+
+    @PostMapping
+    public Book createBook(@RequestBody Book book) {
+        System.out.println("CreateBook");
+        return new Book(book.id(), book.tittle());
+    }
+
+    @PutMapping("/{id}")
+    public Book updateBook(@PathVariable Long id, @RequestBody Book book) {
+        System.out.println("UpdateBook");
+        return new Book(id, book.tittle());
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable Long id) {
+        System.out.println("DeleteBook");
     }
 }
